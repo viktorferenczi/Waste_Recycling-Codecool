920caae9a72978fe1be709ca02e020621830942c
viktorferenczi <ferencziviktor11@gmail.com>
2020-01-14 12:43:06 +0100

    adding

--- waste-recycling/Dustbin.cs
+++ waste-recycling/Dustbin.cs
@@ -3,41 +3,46 @@ namespace WasteRecycling
     public class Dustbin
     {
         string Color { set; get; }
-        PaperGarbage[] PaperContent;
-        PlasticGarbage[] PlasticContent;
-        Garbage[] HouseWasteContent;
-    }
+        PaperGarbage[] PaperContent = new PaperGarbage[20];
+        PlasticGarbage[] PlasticContent = new PlasticGarbage[20];
+        Garbage[] HouseWasteContent = new Garbage[20];
 
-    void DisplayContents()
-    {
-        System.Console.WriteLine(Color + "Dustbin!");
-        System.Console.WriteLine("House waste content: " + Garbage.Lenght + " item(s)");
-        foreach (var item in HouseWasteContent)
+
+        void DisplayContents()
         {
-            System.Console.WriteLine(item);
-        }
-
-        System.Console.WriteLine("Paper content: " + PaperGarbage.Lenght + " item(s)");
-
-        foreach (var item in PaperGarbage)
-        {
-            System.Console.WriteLine(item);
-        }
-
-        System.Console.WriteLine("Plastic conten: " + PlasticGarbage.Lenght + " item(s)");
-
-        foreach (var item in PlasticGarbage)
-        {
-            System.Console.WriteLine(item);
-        }
-    }
-    void ThrowOutGarbage(Garbage garbage)
-    {
-        if (garbage is PlasticGarbage) 
-        {
-            if (((PlasticGarbage)garbage).Cleaned == true)
+            System.Console.WriteLine(Color + "Dustbin!");
+            System.Console.WriteLine("House waste content: " + HouseWasteContent.Length + " item(s)");
+            foreach (var item in HouseWasteContent)
             {
+                System.Console.WriteLine(item);
+            }
 
+            System.Console.WriteLine("Paper content: " + PaperContent.Length + " item(s)");
+
+            foreach (var item in PaperContent)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            System.Console.WriteLine("Plastic content: " + PlasticContent.Length + " item(s)");
+
+            foreach (var item in PlasticContent)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+        void ThrowOutGarbage(Garbage garbage)
+        {
+            if (garbage is PlasticGarbage)
+            {
+                if (((PlasticGarbage)garbage).Cleaned == true)
+                {
+                    int index = -1;
+                    for (int i = 0; i < PlasticContent.Length; i++)
+                    {
+
+                    }
+                }
             }
         }
     }
